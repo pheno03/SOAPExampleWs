10af70b56a3d63a2ead375906bab903cae2650da
pheno03 <46960390+pheno03@users.noreply.github.com>
2019-02-13 17:45:48 +0100

    Exemple web Srvice SOAP

---
+++ SOAPExampleWs/src/com/journaldev/jaxws/beans/Person.java
@@ -0,0 +1,58 @@
+package com.journaldev.jaxws.beans;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String name;
+	private int age;
+	private int id;
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		  return id+"::"+name+"::"+age;
+	}
+	
+	 
+
+}
